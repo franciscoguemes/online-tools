1327947ef248e434153e1605d5d568df5c6ef6b3
Francisco Güemes <francisco.gueemes@uniscon.net>
2020-03-07 18:33:23 +0100

    First commit

---
+++ src/test/java/com/franciscoguemes/samples/onlinetools/OnlineToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franciscoguemes.samples.onlinetools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
